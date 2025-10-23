984a38724b146c749c1cc42282159f40bfa6f84b
Kofi Brandful <rhutaford@gmail.com>
2025-10-23 23:07:24 +0000

    added permissions

---
+++ posts/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        #only Authenticated users can view list
+        if request.user.is_authenticated:
+            return True
+        return False
+    def has_object_permission(self, request, view, obj):
+        #write permissions allowed for superuser or post author
+        #read permissions allowed for allowed user user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
