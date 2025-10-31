fad3046f4229f3082baa673f37ce7388968f1f17
Kofi Brandful <rhutaford@gmail.com>
2025-10-31 11:54:25 +0000

    added permissions

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,29 @@
 
-from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAdminUser
+from rest_framework import viewsets #importing viewsets instead of generics
+#also we change the four classes to just two since viewset makes available both list and detail
+
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer,UserSerializer
 # Create your views here.
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostDetail(viewset.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
